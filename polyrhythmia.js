0154eb29ef6dbc1e51fd590bef60ef690ab663a9
pdv <philipjdelvecchio@gmail.com>
2017-06-23 12:46:25 -0400

    Precomute edges

--- polyrhythmia.js
+++ polyrhythmia.js
@@ -7,6 +7,8 @@
 
 const $ = (id) => document.getElementById(id);
 
+// Global
+
 const colors = {
     red: 'black'
 };
@@ -16,31 +18,8 @@ const circle = {
     center: { x: 0, y: 0 }
 };
 
-const canvas = $('canvas');
-const gctx = canvas.getContext('2d');
-const resize = () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    gctx.canvas.width = width;
-    gctx.canvas.height = height;
-    // gctx.translate(width / 2, height / 2);
-    circle.radius = (Math.min(width, height) / 2) * 0.9;
-    circle.center = { x: width / 2, y: height / 2 };
-
-    console.log('width: ' + width + ', height: ' + height);
-    console.log('radius: ' + circle.radius);
-    console.log('center: {x: ' + circle.center.x + ', y: ' + circle.center.y + '}');
-};
-
-const actx = new window.AudioContext();
-
-const drawCircle = () => {
-    gctx.strokeStyle = colors.red;
-    gctx.lineWidth = 1;
-    gctx.beginPath();
-    gctx.arc(circle.center.x, circle.center.y, circle.radius, 0, 2 * Math.PI);
-    gctx.stroke();
-};
+const points = {}; // [Point: Bool]
+const shapes = []; // [[Point]]
 
 const pointOnCircle = (angle) => {
     return {
@@ -60,29 +39,89 @@ const pointsOnCircle = (numSides) => {
     return points;
 };
 
-const drawShape = (numSides, color) => {
+const loadShapes = () => {
+    for (let numSides = 0; numSides <= 8; numSides++) {
+        const shapePoints = pointsOnCircle(numSides);
+        shapePoints.forEach((point) => { points[point] = false; });
+        shapes[numSides] = shapePoints;
+    }
+};
+
+
+// Canvas setup
+
+const canvas = $('canvas');
+const gctx = canvas.getContext('2d');
+const resize = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    gctx.canvas.width = width;
+    gctx.canvas.height = height;
+    circle.radius = (Math.min(width, height) / 2) * 0.9;
+    circle.center = { x: width / 2, y: height / 2 };
+
+    console.log('width: ' + width + ', height: ' + height);
+    console.log('radius: ' + circle.radius);
+    console.log('center: {x: ' + circle.center.x + ', y: ' + circle.center.y + '}');
+};
+
+
+// Cursor
+
+const actx = new window.AudioContext();
+const loopLength = 2; // secs
+const startTime = actx.currentTime;
+const cursorPos = () => {
+    let elapsedTime = actx.currentTime - startTime;
+    let elapsedLoops = Math.floor(elapsedTime / loopLength);
+    return (elapsedTime - (elapsedLoops * loopLength)) / loopLength;
+};
+const cursorAngle = () => {
+    return cursorPos() * 2 * Math.PI;
+};
+
+const drawCursor = () => {
+    gctx.fillStyle = 'red';
+    gctx.beginPath();
+    const angle = cursorAngle();
+    const point = pointOnCircle(angle);
+    gctx.arc(point.x, point.y, 15, 0, 2 * Math.PI);
+    gctx.fill();
+};
+
+
+// Drawing
+
+const drawCircle = () => {
+    gctx.strokeStyle = colors.red;
+    gctx.lineWidth = 1;
+    gctx.beginPath();
+    gctx.arc(circle.center.x, circle.center.y, circle.radius, 0, 2 * Math.PI);
+    gctx.stroke();
+};
+
+const drawShape = (shape, color) => {
     gctx.strokeStyle = color;
     gctx.lineWidth = 1;
     gctx.beginPath();
-    const points = pointsOnCircle(numSides);
-    gctx.moveTo(points[0].x, points[0].y);
-    points.forEach((point) => { gctx.lineTo(point.x, point.y); });
-    gctx.lineTo(points[0].x, points[0].y);
+    gctx.moveTo(shape[0].x, shape[0].y);
+    shape.forEach((point) => { gctx.lineTo(point.x, point.y); });
+    gctx.lineTo(shape[0].x, shape[0].y);
     gctx.stroke();
 };
 
 const draw = () => {
     gctx.clearRect(0, 0, canvas.width, canvas.height);
     drawCircle();
-    for (let i = 2; i <= 8; i++) {
-        drawShape(i, 'black');
-    }
+    shapes.slice(2).forEach((shape) => { drawShape(shape, 'black'); });
+    drawCursor();
     window.requestAnimationFrame(draw);
 };
 
 const main = () => {
     window.addEventListener('resize', resize, false);
     resize();
+    loadShapes();
     draw();
 };
 
